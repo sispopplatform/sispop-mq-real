ae8d461c1dfb941e0bd5f06bfd74d8c9e68553ce
ml-sispop <131583737+syssiproject@users.noreply.github.com>
2023-09-27 23:59:54 +0000

    dev2

--- sispopmq/connections.h
+++ sispopmq/connections.h
@@ -8,7 +8,7 @@
 #include <utility>
 #include <variant>
 
-namespace lokimq {
+namespace sispopmq {
 
 struct ConnectionID;
 
@@ -77,18 +77,18 @@ private:
     long long id = 0;
     std::string pk;
     std::string route;
-    friend class LokiMQ;
+    friend class SispopMQ;
     friend struct std::hash<ConnectionID>;
     template <typename... T>
     friend bt_dict detail::build_send(ConnectionID to, std::string_view cmd, T&&... opts);
     friend std::ostream& operator<<(std::ostream& o, const ConnectionID& conn);
 };
 
-} // namespace lokimq
+} // namespace sispopmq
 namespace std {
-    template <> struct hash<lokimq::ConnectionID> {
-        size_t operator()(const lokimq::ConnectionID &c) const {
-            return c.sn() ? lokimq::already_hashed{}(c.pk) :
+    template <> struct hash<sispopmq::ConnectionID> {
+        size_t operator()(const sispopmq::ConnectionID &c) const {
+            return c.sn() ? sispopmq::already_hashed{}(c.pk) :
                 std::hash<long long>{}(c.id) + std::hash<std::string>{}(c.route);
         }
     };
